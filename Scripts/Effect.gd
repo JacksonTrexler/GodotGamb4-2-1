d43d3611327b4e182a6d2b6a040297c7ff4ee08e
Jackson <jtrexler25@gmail.com>
2024-06-05 22:03:35 -0400

    Effect changes
    Effect changes

--- Scripts/Effect.gd
+++ Scripts/Effect.gd
@@ -10,6 +10,16 @@ var icon : Sprite2D
 var animation : AnimatedSprite2D
 var sound #What is this?
 
+func _init(_intensity : int = 1, _rounds : int = 0):
+	intensity = _intensity;
+	rounds = _rounds;
+	
+func _initFull(_intensity : int, _rounds : int, _icon : Sprite2D, _animation : AnimatedSprite2D, sound):
+	intensity = _intensity;
+	rounds = _rounds;
+	icon = _icon;
+	animation = _animation;
+
 func apply(target: Unit):
 	applyEffect(target) #Electively implemented
 	#Play sound?
@@ -19,6 +29,9 @@ func applyEffect(target: Unit):
 	print("Effect applied") #This should be implemented
 	
 # Effect Components
-func heal(target: Unit, amount: int):
-	target.HP += amount
+func heal(target: Unit, amount: int, mod = 0):
+	target.HP += amount;
+	
+func hurt(target: Unit, amount: int, mod = 0):
+	target.HP += amount;
 
